9e8fede2878f48104c13ecd5565d6b99ee751d3f
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-06-20 14:21:20 +0530

    Update StartStopTracking.js

--- src/components/StartStopTracking.js
+++ src/components/StartStopTracking.js
@@ -1,1 +1,28 @@
+import React, { useState } from 'react';
+
+const StartStopTracking = ({ onStart, onStop }) => {
+  const [isTracking, setIsTracking] = useState(false);
+
+  const handleStart = () => {
+    setIsTracking(true);
+    onStart();
+  };
+
+  const handleStop = () => {
+    setIsTracking(false);
+    onStop();
+  };
+
+  return (
+    <div>
+      {isTracking ? (
+        <button onClick={handleStop}>Stop Tracking</button>
+      ) : (
+        <button onClick={handleStart}>Start Tracking</button>
+      )}
+    </div>
+  );
+};
+
+export default StartStopTracking;
 
