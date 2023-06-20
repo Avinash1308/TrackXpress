4879d4cc53b84e88527502d3127ec272e6fb9202
Avinash1308 <92975993+Avinash1308@users.noreply.github.com>
2023-06-20 14:12:51 +0530

    Update RealTimeLocationTracking.js

--- src/components/RealTimeLocationTracking.js
+++ src/components/RealTimeLocationTracking.js
@@ -1,1 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+function RealTimeLocationTracking() {
+  const [isTracking, setIsTracking] = useState(false);
+  const [path, setPath] = useState([]);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isTracking) {
+      intervalId = setInterval(() => {
+        // Fetch and update driver's current location
+        // ...
+
+        // Update the path with the new location
+        setPath((prevPath) => [...prevPath, currentLocation]);
+      }, 10000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isTracking]);
+
+  const handleStartTracking = () => {
+    setIsTracking(true);
+  };
+
+  const handleStopTracking = () => {
+    setIsTracking(false);
+  };
+
+  return (
+    <div>
+      <h2>Real-time Location Tracking</h2>
+      <button onClick={handleStartTracking}>Start Tracking</button>
+      <button onClick={handleStopTracking}>Stop Tracking</button>
+      <div>
+        <h3>Path:</h3>
+        <ul>
+          {path.map((location, index) => (
+            <li key={index}>{`Latitude: ${location.latitude}, Longitude: ${location.longitude}`}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default RealTimeLocationTracking;
 
